334d33e197e6e2eb4664df7e9e972172313401e5
Carl Jidling <carl.jidling@it.uu.se>
2021-02-23 17:43:16 +0100

    method for calling C-code

--- gp_strain.py
+++ gp_strain.py
@@ -1,6 +1,7 @@
 import numpy as np
+from ctypes import *
+from numpy.ctypeslib import ndpointer
 
-# covariance function object
 class covfunc():
     """
     Class representing the covariance function
@@ -28,7 +29,25 @@ class covfunc():
         self.nu = nu
 
 
-# main GP class
+def get_mperms(mx, my):
+    """Returns basis function indices
+
+    Parameters
+    ----------
+    mx, my : ints
+        number of basis functions in x/y-directions
+
+    Returns
+    -------
+    mperms : int
+        (mx*my,2) array with all permutations: (1,1), (1,2), ..., (mx,my)
+        that lie inside the ellipse mm1.^2/m_1^2+mm2.^2/m_2^2=1
+    """
+    mX, mY = np.meshgrid(np.arange(1,mx+1), np.arange(1,my+1))
+    insideEllipse = np.where( mX**2/mx**2 + mY**2/my**2 <= 1 )[0]
+    np.stack( (mX.reshape(-1)[insideEllipse], mY.reshape(-1)[insideEllipse]), axis=1)
+
+
 class gp_strain(object):
     """
     Class representing a strain reconstruction object. This class contains necessary
@@ -41,77 +60,128 @@ class gp_strain(object):
     Attributes
     ----------
     obs : float
-        (4,N+totAddSegs) array with entry and exit points for the measurements,
+        (4,nobs+totAddSegs) array with entry and exit points for the measurements,
         each column having the form [x0; x1; y0; y1]. If the ray passes through
         multiple segments, place the columns left to right corresponding to
-        first to last segment. N is the number of measurements and totAddSegs is
-        the total number of additional segments of all measurements (e.g. if a
-        ray passes through 3 segments, 2 of them are considered additional)
+        first to last segment. Here 'nobs' is the number of measurements and
+        'totAddSegs' is the total number of additional segments of all measurements
+        (e.g. if a ray passes through 3 segments, 2 of them are considered additional)
     y : float
-        (N,) array with measured values
+        (nobs,) array with measured values
     pred : float
         (P,2) array with coordinates for the prediction on the form [X(:) Y(:)]
-    m_1, m_2 : ints
-        number of basis functions in x/y-direction
+    mperms : int
+        (m,2) array with all permutations: (1,1), (1,2), ..., (mx,my)
+        that lie inside the ellipse x^2/my^2+y^2/mx^2=1
+    m : int
+        number of basis functions
     Lx, Ly : floats
         domain sizes in x/y-direction for the Laplace eigenvalue problem
     nrSegs - int
-        (N,) array specifying the number of segments the ray passes through for
+        (nobs,) array specifying the number of segments the ray passes through for
         each measurement
-    addPrevSegs : int
-        (N,) vector specifying the total number of additional segments of all
-        previous measurements TODO: derived
     sigma_f : float
         signal variance hyperparameter
     l : float
         lengthscale hyperparameter
     sigma_n : float
         noise std hyperparameter
-    v - Poisson's ratio
     covfunc : covfunc
         covfunc object representing the covariance function
-
+    addPrevSegs : int
+        (nobs,) vector specifying the total number of additional segments of all
+        previous measurements
+    A, B : floats
+        parameters specifying the equilibrium constraint
+    nobs : int
+        number of measurements
+    npred : int
+        number of test points
+    Phi : double
+        (nobs, m) array of basis functions evaluated at measurement inputs
+    Phi_pred_T : double
+        (npred, m) array of basis functions evaluated at test inputs
 
     """
 
     def __init__(self, obs, y, pred, mx, my, Lx, Ly, nrSegs, sigma_f, l, sigma_n, v, covfunc=covfunc()):
+        """
+        Parameters
+        ----------
+        obs : float
+            (4,N+totAddSegs) array with entry and exit points for the measurements,
+            each column having the form [x0; x1; y0; y1]. If the ray passes through
+            multiple segments, place the columns left to right corresponding to
+            first to last segment. N is the number of measurements and totAddSegs is
+            the total number of additional segments of all measurements (e.g. if a
+            ray passes through 3 segments, 2 of them are considered additional)
+        y : float
+            (N,) array with measured values
+        pred : float
+            (P,2) array with coordinates for the prediction on the form [X(:) Y(:)]
+        m_1, m_2 : ints
+            number of basis functions in x/y-direction
+        Lx, Ly : floats
+            domain sizes in x/y-direction for the Laplace eigenvalue problem
+        nrSegs - int
+            (N,) array specifying the number of segments the ray passes through for
+            each measurement
+        sigma_f : float
+            signal variance hyperparameter
+        l : float
+            lengthscale hyperparameter
+        sigma_n : float
+            noise std hyperparameter
+        v : float
+            Poisson's ratio
+        covfunc : covfunc
+            covfunc object representing the covariance function
+        """
         super(gp_strain, self).__init__()
-        # TODO: initialise all variables
         self.obs = obs
         self.y = y
         self.pred = pred
-        self.mperms = self.get_mperms(mx, my)
+        self.mperms = get_mperms(mx, my)
+        self.m = self.mperms.shape[0]
         self.Lx = Lx
         self.Ly = Ly
         self.nrSegs = nrSegs
         self.sigma_f = sigma_f
         self.l = l
         self.sigma_n = sigma_n
-        self.v = v
         self.covfunc = covfunc
 
-
-        def get_mperms(self, mx, my):
-            """Returns basis function indices
-
-            Parameters
-            ----------
-            mx, my : ints
-                number of basis functions in x/y-directions
-
-            Returns
-            -------
-            mperms : int
-                (mx*my,2) array with all permutations: (1,1), (1,2), ..., (mx,my)
-                that lie inside the ellipse mm1.^2/m_1^2+mm2.^2/m_2^2=1
-            """
-            mX, mY = np.meshgrid(np.arange(1,mx+1), np.arange(1,my+1))
-            insideEllipse = np.where( mX**2/mx**2 + mY**2/my**2 <= 1 )[0]
-            return np.stack((mX.reshape(-1)[insideEllipse], mY.reshape(-1)[insideEllipse]),axis=1)
+        self.addPrevSegs = np.concatenate((np.zeros(1), np.cumsum(nrSegs[:-1:]-1))).astype(int)
+        self.A = 1 + (1-v)/v
+        self.B = - (self.A + 1)
+        self.nobs = self.y.size
+        self.npred = self.pred.shape[0]
+        self.Phi = np.zeros((self.nobs, self.m))
+        self.Phi_pred_T = np.zeros((self.npred, self.m))
+        self.build_phi() # fill the Phi-matrices
 
 
-#       def build_phi():
-            # calling c-function
+    def build_phi(self):
+        """Building the Phi-matrices. This is done in an external C-function,
+        to which pointers are passed -- hence this function does not return
+        anything.
+        """
+        cBN = CDLL('./build_phi.so')
+        cBN.build_phi.restype = None
+        cBN.build_phi.argtypes = [c_int, c_int, c_int,
+                                  ndpointer(c_double, flags='C_CONTIGUOUS'),
+                                  ndpointer(c_double, flags='C_CONTIGUOUS'),
+                                  ndpointer(c_double, flags='C_CONTIGUOUS'),
+                                  ndpointer(c_double, flags='C_CONTIGUOUS'),
+                                  c_double, c_double, c_double, c_double,
+                                  ndpointer(c_int, flags='C_CONTIGUOUS'),
+                                  ndpointer(c_int, flags='C_CONTIGUOUS'),
+                                  ndpointer(c_double, flags='C_CONTIGUOUS'),
+                                  ndpointer(c_double, flags='C_CONTIGUOUS')]
+        cBN.build_phi(self.nobs, self.npred, self.m, self.mperms,
+                      self.obs, self.pred[:,0], self.pred[:,1], self.Lx,
+                      self.Ly, self.A, self.B, self.nrSegs, self.addPrevSegs,
+                      self.Phi, self.Phi_pred)
 
 #        def optimiseML():
             # selecting hyperpars by minimising nll
